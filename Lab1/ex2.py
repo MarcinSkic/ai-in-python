464a3a5c92dbcc56c0f833b26a2fe127779b722a
Marcin Skic <marcinskicoficjal@gmail.com>
2022-10-19 23:01:51 +0200

    [ai-in-python] Do exercises 2 and 3 from lab1

---
+++ Lab1/ex2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 11:06:52 2022
+
+@author: marcinskic
+"""
+
+# TASK 1
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+data_excel = pd.read_excel("practice_lab_1.xlsx",engine="openpyxl")
+data_excel = data_excel.iloc[:100,:7]
+
+columnsNames =np.array(list(data_excel.columns))
+data = data_excel.values
+
+even = data[::2,:]
+odd = data[1::2,:]
+subtracted = even-odd
+
+# TASK 2
+mean_task2 = data.mean()
+std_task2 = data.std()
+
+arr_task2 = (data - mean_task2)/std_task2
+
+# TASK 3
+arr_task3 = (data-data.mean(axis=0))/(np.spacing(data.std(axis=0))+data.std(axis=0))
+
+#TASK 4
+changeFactor = data.mean(axis=0) / (np.spacing(data.std(axis=0))+data.std(axis=0))
+
+#TASK 5
+mask_task5 = changeFactor == changeFactor.max()
+arr_task5 = data[:,mask_task5]
+print("Kolumna z największym współczynnikiem zmiany:",columnsNames[mask_task5][0])
+
+#TASK 6
+mask_task6 = data>data.mean(axis=0)
+arr_task6 = np.count_nonzero(mask_task6,axis=0) #Można też mask_task6.sum(axis=0)
+
+#TASK 7
+max_values = data.max(axis=0)
+mask_task7 = max_values == max_values.max()
+print("Kolumy z wartosciami maksymalnymi:",columnsNames[mask_task7])
+
+#TASK 8
+mask_task8_1 = (data==0).sum(axis=0)
+mask_task8_2 = mask_task8_1 == mask_task8_1.max()
+print("Koluma z największą liczbą zer:",columnsNames[mask_task8_2][0])
+
+#TASK 9
+even_sums = data[::2,:].sum(axis=0)
+odd_sums = data[1::2,:].sum(axis=0)
+mask_task9 = even_sums>odd_sums
+print("Kolumny gdzie suma elementów na pozycjach parzystych jest większa:",columnsNames[mask_task9])
---
+++ Lab1/ex3.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 19 22:09:24 2022
+
+@author: marci
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+e = math.e
+
+fig, ax = plt.subplots(3,2, figsize = (10,15))
+x = np.arange(-5, 5, 0.1)
+
+#TASK 1
+y1 = np.tanh(x)
+
+ax[0,0].plot(x,y1)
+ax[0,0].set_xlabel('x')
+ax[0,0].set_ylabel('y')
+
+#TASK 2
+y2 = (e**x - e**(-x))/(e**x+e**(-x))
+
+ax[0,1].plot(x,y2)
+ax[0,1].set_xlabel('x')
+ax[0,1].set_ylabel('y')
+
+#TASK 3
+y3 = 1/1+e**(-x)
+
+ax[1,0].plot(x,y3)
+ax[1,0].set_xlabel('x')
+ax[1,0].set_ylabel('y')
+
+#TASK 4
+y4_1 = x[x <= 0]*0
+y4_2 = x[x > 0]
+y4 = np.concatenate((y4_1,y4_2))
+
+ax[1,1].plot(x,y4)
+ax[1,1].set_xlabel('x')
+ax[1,1].set_ylabel('y')
+
+#TASK 5
+y5_1 = e**(x[x <= 0])-1
+y5_2 = x[x > 0]
+y5 = np.concatenate((y5_1,y5_2))
+
+ax[2,0].plot(x,y5)
+ax[2,0].set_xlabel('x')
+ax[2,0].set_ylabel('y')
