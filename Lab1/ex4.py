c36033d35d56c2cc1afa130fe97f5a932196c0bf
Marcin Skic <marcinskicoficjal@gmail.com>
2022-10-20 17:25:07 +0200

    [ai-in-python] Finish lab1

--- Lab1/ex4.py
+++ Lab1/ex4.py
@@ -11,7 +11,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data_frame = pd.read_excel("practice_lab_1.xlsx",engine="openpyxl")
-test = data_frame.corr()
+corr_arr = data_frame.corr()
 
-for i in test:
-    print (test[i])
+data = data_frame.values
+
+fig, ax = plt.subplots(7,7, figsize=(35,35))
+x = np.arange(0,100,1)
+
+for i in range(7):
+    for z in range(7): #W celu uniknięcia powtórzeń można użyć range(i,7,1)
+        y1 = data[:,i]
+        y2 = data[:,z]
+        ax[i,z].scatter(x,y1)
+        ax[i,z].scatter(x,y2)
+        ax[i,z].set_title("Kolumna {} vs Kolumna {}".format(i+1,z+1))
+        #ax[i,z].set_ylim([-1,100]) opcjonalnie w celu wzrokowego potwierdzenia poprawnosci algorytmu
