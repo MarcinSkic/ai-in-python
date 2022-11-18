b8438a6a86ecfebc76c9adfe20f2d87fcda16ff5
Marcin Skic <marcinskicoficjal@gmail.com>
2022-11-18 14:56:07 +0100

    Start practice for test

---
+++ Lab4/practiceForTest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 14:51:02 2022
+
+@author: marci
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.neighbors.KNeighborsClassifier import kNN
+
